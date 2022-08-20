647a12b2e1d849522f21a868430be46afdbe1b59
Samuel Silveira <samuel.silveira@live.com>
2022-08-20 16:04:04 -0300

    Commit aula03 e aula04

---
+++ poo/aula04/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 4 - Métodos Especiais</title>
+</head>
+
+<body>
+    <pre>
+    <?php
+        require_once 'Caneta.php';
+        
+        $c1 = new Caneta("BIC", "Azul", 0.5);
+        $c2 = new Caneta("KKK", "Verde", 1.0);
+        
+        print_r($c1);
+        print_r($c2);
+    ?>
+    </pre>
+</body>
+
+</html>
